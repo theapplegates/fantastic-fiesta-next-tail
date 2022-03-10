ad1daf053b1c71ec751f5b025cb2c55f0c645db2
Paul <paul@theapplegates.com>
2022-03-10 00:56:39 -0500

    first commit

---
+++ .next/standalone/.next/server/chunks/59.js
@@ -0,0 +1,98 @@
+"use strict";
+exports.id = 59;
+exports.ids = [59];
+exports.modules = {
+
+/***/ 7059:
+/***/ ((module) => {
+
+
+const siteMetadata = {
+    title: 'Next.js Starter Blog',
+    author: 'Tails Azimuth',
+    headerTitle: 'TailwindBlog',
+    description: 'A blog created with Next.js and Tailwind.css',
+    language: 'en-us',
+    theme: 'system',
+    siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+    siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+    siteLogo: '/static/images/logo.png',
+    image: '/static/images/avatar.png',
+    socialBanner: '/static/images/twitter-card.png',
+    email: 'address@yoursite.com',
+    github: 'https://github.com',
+    twitter: 'https://twitter.com/Twitter',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com',
+    linkedin: 'https://www.linkedin.com',
+    locale: 'en-US',
+    analytics: {
+        // If you want to use an analytics provider you have to add it to the
+        // content security policy in the `next.config.js` file.
+        // supports plausible, simpleAnalytics, umami or googleAnalytics
+        plausibleDataDomain: '',
+        simpleAnalytics: false,
+        umamiWebsiteId: '',
+        googleAnalyticsId: ''
+    },
+    newsletter: {
+        // supports mailchimp, buttondown, convertkit, klaviyo, revue
+        // Please add your .env file and modify it according to your selection
+        provider: 'buttondown'
+    },
+    comment: {
+        // If you want to use a commenting system other than giscus you have to add it to the
+        // content security policy in the `next.config.js` file.
+        // Select a provider and use the environment variables associated to it
+        // https://vercel.com/docs/environment-variables
+        provider: 'giscus',
+        giscusConfig: {
+            // Visit the link below, and follow the steps in the 'configuration' section
+            // https://giscus.app/
+            repo: "theapplegates/free-nextjs-random",
+            repositoryId: "R_kgDOG50kgw",
+            category: "General",
+            categoryId: "DIC_kwDOG50kg84CBUp8",
+            mapping: 'pathname',
+            reactions: '1',
+            // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+            metadata: '0',
+            // theme example: light, dark, dark_dimmed, dark_high_contrast
+            // transparent_dark, preferred_color_scheme, custom
+            theme: 'light',
+            // Place the comment box above the comments. options: bottom, top
+            inputPosition: 'bottom',
+            // Choose the language giscus will be displayed in. options: en, es, zh-CN, zh-TW, ko, ja etc
+            lang: 'en',
+            // theme when dark mode
+            darkTheme: 'transparent_dark',
+            // If the theme option above is set to 'custom`
+            // please provide a link below to your custom theme css file.
+            // example: https://giscus.app/themes/custom_example.css
+            themeURL: ''
+        },
+        utterancesConfig: {
+            // Visit the link below, and follow the steps in the 'configuration' section
+            // https://utteranc.es/
+            repo: "",
+            issueTerm: '',
+            label: '',
+            // theme example: github-light, github-dark, preferred-color-scheme
+            // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
+            theme: '',
+            // theme when dark mode
+            darkTheme: ''
+        },
+        disqusConfig: {
+            // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
+            shortname: ""
+        }
+    }
+};
+module.exports = siteMetadata;
+
+
+/***/ })
+
+};
+;
---
+++ pages/about.js
@@ -0,0 +1,49 @@
+import { MDXLayoutRenderer } from '@/components/MDXComponents'
+import { getFileBySlug } from '@/lib/mdx'
+
+const DEFAULT_LAYOUT = 'AuthorLayout'
+
+export async function getStaticProps() {
+  const authorDetails = await getFileBySlug('authors', ['default'])
+  return { props: { authorDetails } }
+}
+
+export default function About({ authorDetails }) {
+  const { mdxSource, frontMatter } = authorDetails
+
+  return (
+    <MDXLayoutRenderer
+      layout={frontMatter.layout || DEFAULT_LAYOUT}
+      mdxSource={mdxSource}
+      frontMatter={frontMatter}
+    />
+  )
+}
+import { DocSearch } from '@docsearch/react'
+import Head from 'next/head'
+
+export const d500dad17ba74bb3877dbcb619af617e = process.env.NEXT_PUBLIC_ALGOLIA_KEY
+export const WJB3BB1LBY = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID
+
+export function Search() {
+  if (!WJB3BB1LBY || !d500dad17ba74bb3877dbcb619af617e) {
+    return null
+  }
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="preconnect"
+          href={`https://${WJB3BB1LBY}-dsn.algolia.net`}
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <DocSearch
+        apiKey={d500dad17ba74bb3877dbcb619af617e}
+        indexName={'nextjs'}
+        appId={WJB3BB1LBY}
+      />
+    </>
+  )
+}
